caa7f0f2c708a2134273eeaee2bae6b8c36fb204
Christiano Quintela <christianoquintela@gmail.com>
2024-01-05 18:54:29 -0300

    Projeto Pet Shop DevMedia.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return <body>
+    <Header />
+    <Banner />
+    <Container_text />
+    <Paralax />
+    <Container_text />
+    <Paralax />
+    <Container_text />
+    <Paralax />
+
+  </body>;
+}
+
+export default App;
