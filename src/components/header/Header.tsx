454bead7bc78ed20c1c7c3af12618a8623e34874
Christiano Quintela <christianoquintela@gmail.com>
2024-01-05 21:45:44 -0300

    Modificado e adicionado imagens do projeto.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import './header.css';
+
+function Header() {
+  return (
+    <header className='container_header'>
+      <div className='container_text'>
+        <h1>Pet Boutique</h1>
+        <p>Estilo e elegância para Cães e Gatos</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- src/teste parallax/parallax.tsx
+++ src/teste parallax/parallax.tsx
@@ -1,116 +1,100 @@
-import { useRef } from 'react'
-import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+//Libs
+import { useRef } from 'react';
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
+//Components
 
-// Little helpers ...
-const url = (name: string, wrap = false) =>
-  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
+//Imgs
+import banho from '../assets/img/banho.png';
 
 export default function App() {
-  const parallax = useRef<IParallax>(null!)
+  const parallax = useRef<IParallax>(null!);
+
   return (
-    <div style={{ width: '100%', height: '100%', background: '#253237' }}>
-      <Parallax ref={parallax} pages={3}>
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+    <section>
+      <div style={{ width: '100%', height: '100vh' }}>
+        <Parallax pages={7}>
+          {/* ******************************************************************************************* */}
+          <ParallaxLayer
+            offset={1}
+            speed={1}
+            style={{ backgroundColor: '#3c184e', zIndex: 1 }}
+          >
+            <h1> - Aqui na Pet Boutique somos apaixonados pelo que fazemos</h1>
+          </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={3}
-          style={{
-            backgroundImage: url('stars', true),
-            backgroundSize: 'cover',
-          }}
-        />
-
-        <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2.5}
-          speed={-0.4}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            pointerEvents: 'none',
-          }}>
-          <img src={url('earth')} style={{ width: '60%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2}
-          speed={-0.3}
-          style={{
-            backgroundSize: '80%',
-            backgroundPosition: 'center',
-            backgroundImage: url('clients', true),
-          }}
-        />
-
-        <ParallaxLayer
-          offset={0}
-          speed={0.1}
-          onClick={() => parallax.current.scrollTo(1)}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <img src={url('server')} style={{ width: '20%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={1}
-          speed={0.1}
-          onClick={() => parallax.current.scrollTo(2)}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <img src={url('bash')} style={{ width: '40%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2}
-          speed={-0}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          onClick={() => parallax.current.scrollTo(0)}>
-          <img src={url('clients-main')} style={{ width: '40%' }} />
-        </ParallaxLayer>
-      </Parallax>
-    </div>
-  )
+          {/* ******************************************************************************************* */}
+          <ParallaxLayer
+            offset={2}
+            speed={1}
+            style={{ backgroundColor: '#3c184e', zIndex: 1 }}
+          >
+            <h1>
+              - Na Pet Boutique você encontra Estilo e Elegância para Cães e
+              Gatos
+            </h1>
+          </ParallaxLayer>
+          {/* ******************************************************************************************* */}
+          <ParallaxLayer
+            offset={0}
+            speed={1}
+            style={{ backgroundColor: '#3c184e', zIndex: 1 }}
+          >
+            <h1>- Header</h1>
+          </ParallaxLayer>
+          {/* ******************************************************************************************* */}
+          <ParallaxLayer
+            offset={0}
+            speed={0}
+            factor={7}
+            style={{
+              backgroundColor: '#fff',
+            }}
+          >
+            <h1>Pet Boutique</h1>
+            <p>Estilo e elegância para Cães e Gatos</p>
+          </ParallaxLayer>
+          {/* ******************************************************************************************* */}
+          <ParallaxLayer
+            offset={0}
+            speed={0.1}
+            onClick={() => parallax.current.scrollTo(1)}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <img src={banho} style={{ width: '40%' }} />
+          </ParallaxLayer>
+          {/* ******************************************************************************************* */}
+          <ParallaxLayer
+            offset={1}
+            speed={0.1}
+            onClick={() => parallax.current.scrollTo(2)}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <img src={banho} style={{ width: '40%' }} />
+          </ParallaxLayer>
+          {/* ******************************************************************************************* */}
+          <ParallaxLayer
+            offset={2}
+            speed={-0}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onClick={() => parallax.current.scrollTo(0)}
+          >
+            <img src={banho} style={{ width: '40%' }} />
+          </ParallaxLayer>
+          {/* ******************************************************************************************* */}
+        </Parallax>
+      </div>
+    </section>
+  );
 }
